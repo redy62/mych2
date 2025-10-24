f4af3910ea1ea5f24a15be10b4979aeb2f41d616
renad <rdz622004@gmail.com>
2025-10-24 03:18:33 +0300

    the first1

---
+++ ch2/SetReminderView.swift
@@ -0,0 +1,18 @@
+//
+//  SetReminderView.swift
+//  ch2
+//
+//  Created by ريناد محمد حملي on 01/05/1447 AH.
+//
+
+import SwiftUI
+
+struct SetReminderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SetReminderView()
+}
---
+++ ch2/TODY.swift
@@ -0,0 +1,18 @@
+//
+//  TODY.swift
+//  ch2
+//
+//  Created by ريناد محمد حملي on 01/05/1447 AH.
+//
+
+import SwiftUI
+
+struct TODY: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TODY()
+}
